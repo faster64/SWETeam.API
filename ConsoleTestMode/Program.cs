bc2fd924a18d2856753320b21a0866c1e85a2dba
Cuong Nguyen <cuongnv.ous@mcredit.com.vn>
2022-07-01 10:41:05 +0700

    console test mode

---
+++ ConsoleTestMode/Program.cs
@@ -0,0 +1,13 @@
+﻿using SWETeam.Common.Caching;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleTestMode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
